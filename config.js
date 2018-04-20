ec67e32ac4dd090a1a540076abf60b5e8d15323d
a-nldisr <1867776+a-nldisr@users.noreply.github.com>
2018-04-20 14:18:56 +0200

    First commit

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+  // currently, showControls does not work nicely with showFooter
+  showControls: false,
+
+  // author, twitter and logo are only shown in case of showFooter
+  showFooter: true,
+  author: "Rogier Dikkes",
+  twitter: "RogierDikkes",
+  logo: "images/download.png"
+};
